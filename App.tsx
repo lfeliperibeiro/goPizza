9bd318ddcf1863dc8ab8dc6801c5571a6fce3882
Felipe Ribeiro <piporibeiro@gmail.com>
2022-01-04 19:55:13 -0300

    chore: add prettier

--- App.tsx
+++ App.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import { View } from 'react-native';
-import {useFonts, DMSans_400Regular} from '@expo-google-fonts/dm-sans'
-import {DMSerifDisplay_400Regular} from '@expo-google-fonts/dm-serif-display'
-import AppLoading from "expo-app-loading";
-
+import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     DMSans_400Regular,
-    DMSerifDisplay_400Regular
+    DMSerifDisplay_400Regular,
   });
 
-  if(!fontsLoaded){
-    return <AppLoading/>
+  if (!fontsLoaded) {
+    return <AppLoading />;
   }
-  return (
-    <View>
-
-    </View>
-  );
+  return <View></View>;
 }
-
